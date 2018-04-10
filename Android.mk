f1982a1299f8fda7c882a47353208050e1113e5a
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-04-09 20:01:53 -0500

    Allow Updater for all
    Change-Id: I361f965804a13222ba251ca3cbf775bdd7645e3d

--- Android.mk
+++ Android.mk
@@ -4,8 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-ifeq ($(AB_OTA_UPDATER),true)
-
 LOCAL_PACKAGE_NAME := Updater
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
@@ -17,5 +15,3 @@ LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
